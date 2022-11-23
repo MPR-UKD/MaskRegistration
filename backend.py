00de52587975fa28296a7dbcddc37f0287bd8dec
ludgerradke <radke@intervall.io>
2022-11-23 11:54:20 +0100

    Init transformation function for registration between two Dicom images and transformation of one mask to the other.

---
+++ backend.py
@@ -0,0 +1,67 @@
+import tempfile
+
+import SimpleITK as sitk
+
+from utils import *
+
+
+def transform(input_dicom_folder_1: Path,
+              input_mask_file: Path,
+              input_dicom_folder_2: Path,
+              out_nii_file: Path,
+              reverse: bool = False):
+
+    # init SimpleITK reader and writer
+    reader = sitk.ImageSeriesReader()
+    writer = sitk.ImageFileWriter()
+
+    #
+    reverse = False if reverse is None else reverse
+
+    while True:
+        dicom_names = split_dcm(reader.GetGDCMSeriesFileNames(input_dicom_folder_2))[0]
+        if reverse:
+            dicom_names = dicom_names[::-1]
+
+        reader.SetFileNames(dicom_names)
+        target = reader.Execute()
+
+        temp_dir_mask_as_dcm = tempfile.TemporaryDirectory()
+
+        mask_to_dicom(input_dicom_folder_1, input_mask_file, temp_dir_mask_as_dcm.name)
+        # read mask_file
+        reader.SetFileNames(reader.GetGDCMSeriesFileNames(temp_dir_mask_as_dcm))
+        mask = reader.Execute()
+        # cast to float for registration
+        mask = sitk.Cast(mask, sitk.sitkFloat32)
+        target = sitk.Cast(target, sitk.sitkFloat32)
+
+        # Registration of mask_as_dcm to input_dicom_folder_2
+        resampleFilter = sitk.ResampleImageFilter()
+        resampleFilter.SetInterpolator(sitk.sitkNearestNeighbor)
+
+        # Transform mask_image
+        resampleFilter.SetSize(target.GetSize())
+        resampleFilter.SetOutputOrigin(target.GetOrigin())
+        resampleFilter.SetOutputSpacing(target.GetSpacing())
+        resampleFilter.SetOutputDirection(target.GetDirection())
+        resampleFilter.SetOutputPixelType(sitk.sitkInt8)
+        resampleFilter.SetDefaultPixelValue(0.0)
+
+        registeredImg = resampleFilter.Execute(mask)
+        registeredImg = sitk.Cast(registeredImg, sitk.sitkUInt8)
+        writer.SetFileName(out_nii_file)
+
+        writer.Execute(registeredImg)
+        img_nifti = nib.load(out_nii_file)
+        img = img_nifti.get_data()
+        if reverse:
+            break
+        if img.max() == 0:
+            reverse = True
+        else:
+            break
+    img_nifti = nib.Nifti1Image(img, img_nifti.affine, img_nifti.header)
+    nib.save(img_nifti, out_nii_file)
+    temp_dir_mask_as_dcm.cleanup()
+
---
+++ utils.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+
+import natsort
+import nibabel as nib
+import numpy as np
+import pydicom
+
+
+def split_dcm(dcm_list: list):
+    locations = {}
+    for f in dcm_list:
+        try:
+            d = pydicom.dcmread(f)
+        except BaseException:
+            continue
+        if d['SliceLocation'].value in locations.keys():
+            locations[d['SliceLocation'].value].append(f)
+        else:
+            locations[d['SliceLocation'].value] = [f]
+    split_dcmList = [locations[key] for key in locations.keys()]
+    echo_list = [[] for _ in range(len(split_dcmList[0]))]
+    keys = list(locations.keys())
+    keys.sort()
+    for key in keys:
+        echos = locations[key]
+        for idx in range(len(echo_list)):
+            echo_list[idx].append(echos[idx])
+    return echo_list
+
+
+def mask_to_dicom(dcm_folder: Path,
+                  nii_file: Path,
+                  out_folder: Path):
+    mask = np.transpose(np.array(nib.load(nii_file).dataobj), (1, 0, 2))
+    dicom_files = natsort.natsorted([_ for _ in dcm_folder.glob('*.dcm')])
+    mask = mask.astype('uint16')
+    for i, dcm_file in enumerate(dicom_files):
+        ds = pydicom.dcmread(dcm_file)
+        ds.PixelData = mask[:, :, i].tobytes()
+        ds.save_as(out_folder / os.path.basename(dcm_file))
+
+
+def check_transform_mask(org_mask: np.ndarray,
+                         transform_mask: np.ndarray):
+    """
+    Check that all regions are present after interpolation / registration
+    """
+    unique_org_mask = np.unique(org_mask)
+    unique_transform_mask = np.unique(transform_mask)
+    b = 2
