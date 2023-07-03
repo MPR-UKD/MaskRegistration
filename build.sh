8be4a26b33187f8c33e634d6f877eb8d2982b590
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-07-03 08:11:48 +0200

    Build exe files and whl file

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# PyInstaller for onefile CLI
+poetry run pyinstaller --onefile --windowed --collect-submodules=pydicom --add-data="./src/MaskRegistration/loading.gif;."  --noconsole src/MaskRegistration/MaskRegistration.py --noconfirm
+
+# PyInstaller for onefile GUI
+poetry run pyinstaller --onefile --collect-submodules=pydicom --windowed --add-data="./src/MaskRegistration/loading.gif;." --noconsole src/MaskRegistration/MaskRegistrationGUI.py --noconfirm
