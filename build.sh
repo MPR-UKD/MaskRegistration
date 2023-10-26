49e146c4d9b5cf93bc3ca62c6b78a4cd4d3542ba
Ludger Radke <113907719+ludgerradke@users.noreply.github.com>
2023-10-26 16:30:41 +0200

    Version updated

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # PyInstaller for onefile CLI
 poetry run pyinstaller --onefile --windowed --collect-submodules=pydicom --add-data="./src/MaskRegistration/loading.gif;."  --noconsole src/MaskRegistration/MaskRegistration.py --noconfirm
 
